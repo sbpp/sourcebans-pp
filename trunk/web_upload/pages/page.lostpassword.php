630dd9855174a51f8b15e3d44bd09eab8fbdd6e7
januk@freenet.de <januk@freenet.de@e5579234-f3d2-11de-99d0-77da0b7ae2c6>
2010-05-18 20:05:30 +0000

    * Fixed Lost Password function :S

--- trunk/web_upload/pages/page.lostpassword.php
+++ trunk/web_upload/pages/page.lostpassword.php
@@ -13,23 +13,23 @@
  * =============================================================================
  */
 
-global $theme;
+global $theme, $userbank;
 
 if(isset($_GET['validation'],$_GET['email']) && !empty($_GET['email']) && !empty($_GET['validation']))
 {  
 	$email = $_GET['email'];
 	$validation = $_GET['validation'];
 	$q = $GLOBALS['db']->GetRow("SELECT * FROM `" . DB_PREFIX . "_admins` WHERE `email` = ? && `validate` = ?", array($email, $validation));
-	if($q[0])
+	if($q)
 	{
 		$newpass = generate_salt(MIN_PASS_LENGTH+1);
 		$query = $GLOBALS['db']->Execute("UPDATE `" . DB_PREFIX . "_admins` SET `password` = '" . $userbank->encrypt_password($newpass) . "' WHERE `email` = ?", array($email));
 		$query = $GLOBALS['db']->Execute("UPDATE `" . DB_PREFIX . "_admins` SET `validate` = '' WHERE `email` = ?", array($email));
 		echo '<div id="msg-blue" style="">
-			<i><img src="./images/info.png" alt="Warning" /></i>
+			<i><img src="./images/info.png" alt="Info" /></i>
 			<b>Password Reset</b>
 			<br />
-			Your password has been reset to<br /><br /><b> '.$newpass.'</b><br />Please login using this password, then use the change password link in Your Account
+			Your password has been reset to<br /><br /><b> '.$newpass.'</b><br />Please login using this password, then use the change password link in Your Account.
 			</div>';
 	}
 	else 
@@ -38,7 +38,7 @@ if(isset($_GET['validation'],$_GET['email']) && !empty($_GET['email']) && !empty
 			<i><img src="./images/warning.png" alt="Warning" /></i>
 			<b>Error</b>
 			<br />
-			The validation string does not match the email for this reset request.</i>
+			The validation string does not match the email for this reset request.
 			</div>';
 	}
 }else 
