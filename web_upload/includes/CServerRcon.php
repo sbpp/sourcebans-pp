2b0aa173485bf23f22a8e6d4736f3042b441849f
smithxxl <smithxxl@e5579234-f3d2-11de-99d0-77da0b7ae2c6>
2010-11-12 18:56:19 +0000

    * CServerInfo & CServerRcon classes updated to support BIND_IP for web servers with strict firewall/IP policies

--- web_upload/includes/CServerRcon.php
+++ web_upload/includes/CServerRcon.php
@@ -12,79 +12,128 @@
  * @version $Id: CServerRcon.php 117 2008-08-21 17:17:54Z peace-maker $
  * =============================================================================
  */
-define("SERVERDATA_EXECCOMMAND", 02);
-define("SERVERDATA_AUTH", 03);
-define("SERVERDATA_RESPONSE_VALUE", 00);
-define("SERVERDATA_AUTH_RESPONSE", 02);
 
-class CServerRcon {
-    var $password;
-    var $_sock = null;
-    var $_id = 0;
+class CServerRcon
+{
+  private $password;
+  private $_sock = null;
+  private $_id = 0;
+  private $isfsock = true;
 
-    function CServerRcon ($address,$port,$password) {
-			$this->password = $password;
-			$this->_sock = @fsockopen($address,$port, $errno, $errstr, 2);
-			stream_set_timeout($this->_sock, 2);
+  const SERVERDATA_EXECCOMMAND = 02;
+  const SERVERDATA_AUTH = 03;
+  const SERVERDATA_RESPONSE_VALUE = 00;
+  const SERVERDATA_AUTH_RESPONSE = 02;
+
+  function CServerRcon ($address, $port, $password)
+  {
+    $this->password = $password;
+
+    try
+    {
+      if (defined('BIND_IP') && function_exists('socket_create') && function_exists('socket_bind'))
+      {
+        $this->isfsock = false;
+        $this->_sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+        socket_set_option($this->_sock, SOL_SOCKET, SO_REUSEADDR, 1);
+        socket_bind($this->_sock, BIND_IP);
+
+        socket_connect($this->_sock, $address, $port);
+
+        socket_set_option($this->_sock, SOL_SOCKET, SO_SNDTIMEO, array("sec"=>2, "usec"=>0));
+        socket_set_option($this->_sock, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>2, "usec"=>0));
+      }
+      else
+      {
+        $this->_sock = @fsockopen($address, $port, $errno, $errstr, 2);
+        stream_set_timeout($this->_sock, 2);
+      }
     }
+    catch (Exception $err) { }
+  }
     
-    function Auth () {
-			$PackID = $this->_write(SERVERDATA_AUTH,$this->password);
-			$ret = $this->_PacketRead();
-			return (isset($ret[1]['ID']) && $ret[1]['ID'] == -1)?0:1;
+  public function Auth ()
+  {
+    $PackID = $this->_Write(CServerRcon::SERVERDATA_AUTH,$this->password);
+    $ret = $this->_PacketRead();
+
+    return (isset($ret[1]['ID']) && $ret[1]['ID'] == -1)?0:1;
+  }
+
+  private function _Write($cmd, $s1='', $s2='')
+  {
+    $id = ++$this->_id;
+    $data = pack("VV",$id,$cmd).$s1.chr(0).$s2.chr(0);
+    $data = pack("V",strlen($data)).$data;
+
+    if ($this->isfock)
+      fwrite($this->_sock, $data, strlen($data));
+    else
+      socket_write($this->_sock, $data, strlen($data));
+
+    return $id;
+  }
+
+  private function _sock_read($size)
+  {
+    if ($this->isfock)
+      return @fread($this->_sock, $size);
+    else
+      return socket_read($this->_sock, $size);
+  }
+
+  private function _PacketRead()
+  {
+    $retarray = array();
+
+    while ($size = $this->_sock_read(4)) 
+    {
+      $size = unpack('V1Size',$size);
+
+      if ($size["Size"] > 4096)
+        $packet = "\x00\x00\x00\x00\x00\x00\x00\x00".$this->_sock_read(4096);
+      else 
+        $packet = $this->_sock_read($size["Size"]);
+
+      array_push($retarray,unpack("V1ID/V1Reponse/a*S1/a*S2",$packet));
     }
 
-    function _Write($cmd, $s1='', $s2='') {
-			$id = ++$this->_id;
-			$data = pack("VV",$id,$cmd).$s1.chr(0).$s2.chr(0);
-			$data = pack("V",strlen($data)).$data;
-			fwrite($this->_sock,$data,strlen($data));
-			return $id;
+    return $retarray;
+  }
+
+  public function Read()
+  {
+    $Packets = $this->_PacketRead();
+
+    foreach($Packets as $pack) 
+    {
+      if (isset($ret[$pack['ID']])) 
+      {
+        $ret[$pack['ID']]['S1'] .= $pack['S1'];
+        $ret[$pack['ID']]['S2'] .= $pack['S1'];
+      }
+      else
+      {
+        $ret[$pack['ID']] = array('Reponse' => $pack['Reponse'],
+                                  'S1' => $pack['S1'],
+                                  'S2' =>	$pack['S2'],);
+      }
     }
 
-    function _PacketRead() {
-			$retarray = array();
-			while ($size = @fread($this->_sock,4)) 
-			{
-			    $size = unpack('V1Size',$size);
-			    if ($size["Size"] > 4096)
-						$packet = "\x00\x00\x00\x00\x00\x00\x00\x00".fread($this->_sock,4096);
-			    else 
-						$packet = fread($this->_sock,$size["Size"]);
-			    array_push($retarray,unpack("V1ID/V1Reponse/a*S1/a*S2",$packet));
-			}
-			return $retarray;
-    }
+    return $ret;
+  }
 
-    function Read() {
-			$Packets = $this->_PacketRead();	
-			foreach($Packets as $pack) 
-			{
-			    if (isset($ret[$pack['ID']])) 
-			    {
-					$ret[$pack['ID']]['S1'] .= $pack['S1'];
-					$ret[$pack['ID']]['S2'] .= $pack['S1'];
-			    } else {
-						$ret[$pack['ID']] = array(
-							'Reponse' => $pack['Reponse'],
-							'S1' => $pack['S1'],
-							'S2' =>	$pack['S2'],
-					    );
-			    }
-			}
-			return $ret;
-    }
+  public function sendCommand($command)
+  {
+    //$command = '"'.trim(str_replace(' ','" "', $command)).'"';
+    $this->_Write(CServerRcon::SERVERDATA_EXECCOMMAND,$command,'');
+  }
 
-    function sendCommand($command) {
-			//$command = '"'.trim(str_replace(' ','" "', $command)).'"';
-			$this->_Write(SERVERDATA_EXECCOMMAND,$command,'');
-    }
-
-    function rconCommand($command) {
-			$this->sendCommand($command);
-			$ret = $this->Read();
-			return $ret[2]['S1'];
-    }
+  public function rconCommand($command)
+  {
+	  $this->sendCommand($command);
+	  $ret = $this->Read();
+	  return $ret[2]['S1'];
+  }
 }
-
-?>
