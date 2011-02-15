d9bb2bbde5fc4155d77c2e525ed0809c3ee48581
erik@interwavestudios.com <erik@interwavestudios.com@e5579234-f3d2-11de-99d0-77da0b7ae2c6>
2011-02-15 13:21:04 +0000

    Installer fixes

--- branches/sourcebans-1.5/web_upload/includes/CUpdate.php
+++ branches/sourcebans-1.5/web_upload/includes/CUpdate.php
@@ -1,107 +1,103 @@
 <?php
 /**
- * =============================================================================
- * Updater Class
- * 
- * @author SteamFriends Development Team
- * @version 1.2.0
- * @copyright SourceBans (C)2007 SteamFriends.com.  All rights reserved.
- * @package SourceBans
- * @link http://www.sourcebans.net
- * 
- * @version $Id$
- * =============================================================================
+ * Updater class
+ *
+ * @author     InterWave Studios
+ * @copyright  SourceBans (C)2007-2011 InterWaveStudios.com. All rights reserved.
+ * @link       http://www.sourcebans.net
+ * @package    SourceBans
+ * @subpackage Updater
+ * @version    $Id$
  */
- 
- class CUpdater
- {
-	var $store=0;
-	
-	function CUpdater()
-	{
-		if(!is_numeric($this->getCurrentRevision()))
-		{			
-			$this->_updateVersionNumber(0); // Set at 0 initially, this will cause all database updates to be run
-		}
-		else if($this->getCurrentRevision() == -1) // They have some fubar version fix it for them :|
-		{
-			$GLOBALS['db']->Execute("INSERT INTO `".DB_PREFIX."_settings` (`setting`, `value`) VALUES ('config.version', '0')");
-		}
-	}
-	
-	function getLatestPackageVersion()
-	{
-		$retval = 0;
-		foreach($this->_getStore() as $version => $key)
-		{
-			if( $version > $retval )
-				$retval = $version;
-		}
-		return $retval;
-	}
-	
-	function doUpdates()
-	{
-		$retstr = "";
-		$error = false;
-		$i = 0;
-		foreach($this->_getStore() as $version => $key)
-		{
-			if( $version > $this->getCurrentRevision() )
-			{
-				$i++;
-				$retstr .= "Running update: <b>v" . $version . "</b>... ";
-				if( !include (ROOT . "updater/data/" . $key))
-				{
-					// OHSHI! Something went tits up :(
-					$retstr .= "<b>Error executing: /updater/data/" . $key . ". Stopping Update!</b>";
-					$error = true;
-					break;
-				}
-				else
-				{
-					// File was executed successfully 
-					$retstr .= "Done.<br /><br />";
-					$this->_updateVersionNumber($version);
-				}
-			}
-		}
-		if( $i == 0 )
-			$retstr .= "<br />Nothing to update...";
-		else
-		{
-			if(!$error)
-				$retstr .= "<br />Updated successfully. Please delete the /updater folder.";
-			else
-				$retstr .= "<br />Update Failed.";
-		}
-		return $retstr;
-	}
-	
-	function getCurrentRevision()
-	{
-		return (isset($GLOBALS['config']['config.version']))?$GLOBALS['config']['config.version']:-1;
-	}
-	
-	function needsUpdate()
-	{
-		return($this->getLatestPackageVersion() > $this->getCurrentRevision());
-	}
-	
-	function _getStore()
-	{
-		if($this->store==0)
-			return include ROOT . "/updater/store.php";
-		else
-			return $this->store;
-	}
-	
-	function _updateVersionNumber($rev)
-	{
-		$ret = $GLOBALS['db']->Execute("UPDATE ".DB_PREFIX."_settings SET value = ? WHERE setting = 'config.version';", array((int)$rev));
-		return !(empty($ret));
-	}
-	
- }
-
-?>
+class CUpdater
+{
+  function __construct()
+  {
+    $version = $this->getCurrentVersion();
+    // If current version is not set, set to 0 to run all updates
+    if(empty($version))
+    {
+      $this->setCurrentVersion(0);
+    }
+  }
+  
+  
+  public function update()
+  {
+    // If already fully updated, ignore
+    if($this->getLatestVersion() <= $this->getCurrentVersion())
+      return array();
+    
+    $updates = array();
+    foreach($this->_data() as $version)
+    {
+      // If update was already applied, ignore
+      if($version <= $this->getCurrentVersion())
+        continue;
+      
+      $updates[$version] = include(ROOT . 'updater/data/' . $version . '.php');
+      // If update was not executed successfully, stop updating
+      if(!$updates[$version])
+        break;
+      
+      // Otherwise, update current version number
+      $this->setCurrentVersion($version);
+    }
+    
+    return $updates;
+  }
+  
+  public function getCurrentVersion()
+  {
+    if(isset($GLOBALS['config']['config.version']))
+      return $GLOBALS['config']['config.version'];
+    
+    return null;
+  }
+  
+  public function getLatestVersion()
+  {
+    static $latest = 0;
+    
+    if(!$latest)
+    {
+      foreach($this->_data() as $version)
+      {
+        if($version <= $latest)
+          continue;
+        
+        $latest = $version;
+      }
+    }
+    
+    return $latest;
+  }
+  
+  public function setCurrentVersion($version)
+  {
+    $ret = $GLOBALS['db']->Execute(
+      'INSERT INTO ' . DB_PREFIX . '_settings (setting, value)
+       VALUES ("config.version", ?)
+       ON DUPLICATE KEY UPDATE value = VALUES(value)',
+       array($version)
+    );
+    
+    return !empty($ret);
+  }
+  
+  
+  protected function _data()
+  {
+    static $data = array();
+    
+    if(empty($data))
+    {
+      foreach(glob(ROOT . 'updater/data/*.php') as $file)
+      {
+        $data[] = pathinfo($file, PATHINFO_FILENAME);
+      }
+    }
+    
+    return $data;
+  }
+}
