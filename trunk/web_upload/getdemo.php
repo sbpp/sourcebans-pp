013cea09dac1bcd84386cd96525e5300cd2a9308
januk@freenet.de <januk@freenet.de@e5579234-f3d2-11de-99d0-77da0b7ae2c6>
2012-08-25 22:10:59 +0000

    * Added filesize of demo on download to enable browsers estimate the download time * Fixed trying to use an invalid socket in CServerInfo

--- trunk/web_upload/getdemo.php
+++ trunk/web_upload/getdemo.php
@@ -13,13 +13,31 @@
  * =============================================================================
  */
 
-include_once("init.php");
+require_once("init.php");
 
-$GLOBALS['db']->Execute("SET NAMES utf8"); 
-$st = $GLOBALS['db']->Prepare("SELECT `filename`, `origname` FROM ".DB_PREFIX."_demos WHERE demtype=? and demid=?");
-$res = $GLOBALS['db']->Execute($st,array($_GET['type'],$_GET['id']));
+if(!isset($_GET['id']) || !isset($_GET['type']))
+  die('No id or type parameter.');
+
+if(strcasecmp($_GET['type'], "B") != 0 && strcasecmp($_GET['type'], "S") != 0)
+  die('Bad type');
+
+$id = (int)$_GET['id'];
+
+$demo = $GLOBALS['db']->GetRow("SELECT filename, origname FROM `".DB_PREFIX."_demos` WHERE demtype=? AND demid=?;", array($_GET['type'], $id));
+
+if(!$demo)
+{
+  die('Demo not found.');
+}
+
+if(!file_exists(SB_DEMOS . "/" . $demo['filename']))
+{
+  die('File not found.');
+}
+
 header('Content-type: application/force-download');
 header('Content-Transfer-Encoding: Binary');
-header('Content-disposition: attachment; filename="'.$res->fields[1].'"');
-@readfile(SB_DEMOS . "/" . $res->fields[0]);
+header('Content-disposition: attachment; filename="' . $demo['origname'] . '"');
+header("Content-Length: " . filesize(SB_DEMOS . "/" . $demo['filename']));
+readfile(SB_DEMOS . "/" . $demo['filename']);
 ?>
