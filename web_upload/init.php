2a2619770985e1d6df0a4896bf4f45876e9ea911
Sarabveer Singh <sarabveer@outlook.com>
2014-11-15 16:02:32 -0500

    Tagged as 1.4.12
    (15/11/14): Version 1.4.12
    -----------------------
    01. * Added Steam3 Support
    02. ? Optimized and updated IpToCountry.csv

--- web_upload/init.php
+++ web_upload/init.php
@@ -83,8 +83,8 @@ if(!defined("DEVELOPER_MODE") && !defined("IS_UPDATE") && file_exists(ROOT."/upd
 // ---------------------------------------------------
 #define('SB_SVN', true);
 if(!defined('SB_VERSION')){
-	define('SB_VERSION', '1.4.11');
-	define('SB_REV', '$Rev: 462 $');
+	define('SB_VERSION', '1.4.12');
+	define('SB_REV', '$Rev: 463 $');
 }
 define('LOGIN_COOKIE_LIFETIME', (60*60*24*7)*2);
 define('COOKIE_PATH', '/');
--- web_upload/themes/default/theme.conf.php
+++ web_upload/themes/default/theme.conf.php
@@ -17,7 +17,7 @@ define('theme_name', "SourceBans Default Theme");
 define('theme_author', "SourceBans Development Team");
 
 // Set the version of the theme here
-define('theme_version', "1.4.11");
+define('theme_version', "1.4.12");
 
 // Set the link of the theme here
 define('theme_link', "http://www.sourcebans.net");
