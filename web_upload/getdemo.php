e1bd9301630a4ad444b803f98eabc6b840924638
Sarabveer Singh <sarabveer@outlook.com>
2014-12-07 15:22:59 -0500

    Added Sucurity Measure for LFI exploit
    https://code.google.com/p/sourcebans/source/detail?r=165#

--- web_upload/getdemo.php
+++ web_upload/getdemo.php
@@ -30,7 +30,9 @@ if(!$demo)
   die('Demo not found.');
 }
 
-if(!file_exists(SB_DEMOS . "/" . $demo['filename']))
+$demo['filename'] = basename($demo['filename']);
+
+if(!in_array($demo['filename'], scandir(SB_DEMOS)) || if(!file_exists(SB_DEMOS . "/" . $demo['filename']))
 {
   die('File not found.');
 }
