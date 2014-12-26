f76a1e64e5355f4eee3895e642c9c4227b1f919b
Sarabveer Singh <sarabveer@outlook.com>
2014-12-26 17:11:32 -0500

    Added Synergy to the Mod List

---
+++ web_upload/updater/data/476.php
@@ -0,0 +1,10 @@
+<?php
+	$temp = $GLOBALS['db']->GetAll("SELECT * FROM `".DB_PREFIX."_mods` WHERE `modfolder` = 'synergy';");
+	if(count($temp) == 0)
+	{
+		$ret = $GLOBALS['db']->Execute('INSERT INTO `'.DB_PREFIX.'_mods` (`name`, `icon`, `modfolder`) VALUES ("Synergy", "synergy.png", "synergy");');
+		if(!$ret)
+			return false;
+	}
+	return true;
+?>
