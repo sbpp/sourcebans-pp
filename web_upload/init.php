74d9ec5df1d16a92cb282e0f86ed17d24ee82429
Sarabveer Singh <sarabveer@outlook.com>
2015-01-06 15:40:21 -0500

    Fixed getdemo.php error
    https://forums.alliedmods.net/showpost.php?p=2245686&postcount=154

--- web_upload/init.php
+++ web_upload/init.php
@@ -84,7 +84,7 @@ if(!defined("DEVELOPER_MODE") && !defined("IS_UPDATE") && file_exists(ROOT."/upd
 define('SB_SVN', true);
 if(!defined('SB_VERSION')){
 	define('SB_VERSION', '1.5.1F-dev');
-	define('SB_REV', '$Rev: 481 $');
+	define('SB_REV', '$Rev: 482 $');
 }
 define('LOGIN_COOKIE_LIFETIME', (60*60*24*7)*2);
 define('COOKIE_PATH', '/');
