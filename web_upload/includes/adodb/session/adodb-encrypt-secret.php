f1d4a8e2949fd028ec193661ec0becd443eef359
Sarabveer Singh <sarabveer@outlook.com>
2015-01-29 22:21:52 -0500

    Updated ADOdb to 5.19, Updated TinyMCE to 3.5.11

--- web_upload/includes/adodb/session/adodb-encrypt-secret.php
+++ web_upload/includes/adodb/session/adodb-encrypt-secret.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
-V5.18 3 Sep 2012  (c) 2000-2012 John Lim (jlim#natsoft.com). All rights reserved.
-         Contributed by Ross Smith (adodb@netebb.com). 
+V5.19  23-Apr-2014  (c) 2000-2014 John Lim (jlim#natsoft.com). All rights reserved.
+         Contributed by Ross Smith (adodb@netebb.com).
   Released under both BSD license and Lesser GPL library license.
   Whenever there is any discrepancy between the two licenses,
   the BSD license will take precedence.
@@ -44,5 +44,3 @@ class ADODB_Encrypt_Secret {
 }
 
 return 1;
-
-?>
