19fe1fd85615bb1323dec6580433bf06e622afaf
Sarabveer Singh <Sarabveer@users.noreply.github.com>
2015-02-10 08:49:45 -0500

    We Forgot to Update this thing!
    Oops....

--- web_upload/init.php
+++ web_upload/init.php
@@ -81,11 +81,11 @@ if(!defined("DEVELOPER_MODE") && !defined("IS_UPDATE") && file_exists(ROOT."/upd
 // ---------------------------------------------------
 //  Initial setup
 // ---------------------------------------------------
-# SourceBans REV: 492 #
+# SourceBans REV: 505 #
 define('SB_GIT', true);
 if(!defined('SB_VERSION')){
 	define('SB_VERSION', '1.5.2F-dev');
-	define('SB_GITRev', '$Git: 151 $');
+	define('SB_GITRev', '$Git: 165 $');
 }
 define('LOGIN_COOKIE_LIFETIME', (60*60*24*7)*2);
 define('COOKIE_PATH', '/');
