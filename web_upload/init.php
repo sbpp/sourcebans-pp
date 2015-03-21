45535caea6523750de9a5f7f83d6be10bd992060
Sarabveer Singh <sarabveer@outlook.com>
2015-03-20 20:03:08 -0400

    Fixed DataPack Error
    Fixed Issue #14
    Compiled with SM 1.7.1-git5165

--- web_upload/init.php
+++ web_upload/init.php
@@ -81,11 +81,11 @@ if(!defined("DEVELOPER_MODE") && !defined("IS_UPDATE") && file_exists(ROOT."/upd
 // ---------------------------------------------------
 //  Initial setup
 // ---------------------------------------------------
-# SourceBans REV: 509 #
+# SourceBans REV: 510 #
 define('SB_GIT', true);
 if(!defined('SB_VERSION')){
 	define('SB_VERSION', '1.5.2F-dev');
-	define('SB_GITRev', '$Git: 169 $');
+	define('SB_GITRev', '$Git: 170 $');
 }
 define('LOGIN_COOKIE_LIFETIME', (60*60*24*7)*2);
 define('COOKIE_PATH', '/');
