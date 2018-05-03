da84729c6e136ac53bd94d9854623a96d588f533
Marvin <Groruk@uberdoge.network>
2018-05-03 07:11:24 +0200

    Add fontawesome locally

--- web/install/template/header.php
+++ web/install/template/header.php
@@ -11,7 +11,7 @@ if (!defined("IN_SB")) {
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>SourceBans++</title>
         <link href="includes/main.css" rel="stylesheet" type="text/css" />
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <script type="text/javascript" src="./scripts/fontawesome-all.min.js"></script>
         <script type="text/javascript" src="./scripts/mootools.js"></script>
         <script type="text/javascript" src="./scripts/sourcebans.js"></script>
         <link rel="Shortcut Icon" href="./images/favicon.ico">
