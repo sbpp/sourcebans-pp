79fcb7fbe4b6cd0340704701e163d30805b8e148
Marvin Lukaschek <Groruk@uberdoge.network>
2018-10-11 20:52:05 +0200

    Rename Cache directory

--- web/updater/index.php
+++ web/updater/index.php
@@ -35,7 +35,7 @@ $theme->assign('updates', $updater->getMessageStack());
 $theme->display('updater.tpl');
 
 //clear compiled themes
-$cachedir = dir(SB_THEMES_COMPILE);
+$cachedir = dir(SB_CACHE);
 while (($entry = $cachedir->read()) !== false) {
     if (is_file($cachedir->path . $entry)) {
         unlink($cachedir->path . $entry);
