00ce5c3b31b6e80843a4bf678c0bee5b700a6094
geominorai <geominorai@users.noreply.github.com>
2019-02-17 23:55:00 -0800

    Fixed updater query overlap

--- web/updater/data/700.php
+++ web/updater/data/700.php
@@ -1,17 +1,5 @@
 <?php
 
-$this->dbs->query(
-    "CREATE TABLE IF NOT EXISTS `:prefix_login_tokens` (
-        `jti` varchar(16) NOT NULL,
-        `secret` varchar(64) NOT NULL,
-        `lastAccessed` int(11) NOT NULL,
-        PRIMARY KEY (`jti`),
-        UNIQUE KEY `secret` (`secret`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=:charset;"
-);
-
-$this->dbs->bind(':charset', DB_CHARSET);
-
 $auth = [
     'auth.maxlife' => 1440,
     'auth.maxlife.remember' => 10080,
@@ -24,5 +12,17 @@ foreach ($auth as $setting => $value) {
     $this->dbs->bind(':value', $value);
     $this->dbs->execute();
 }
+
+$this->dbs->query(
+    "CREATE TABLE IF NOT EXISTS `:prefix_login_tokens` (
+        `jti` varchar(16) NOT NULL,
+        `secret` varchar(64) NOT NULL,
+        `lastAccessed` int(11) NOT NULL,
+        PRIMARY KEY (`jti`),
+        UNIQUE KEY `secret` (`secret`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=:charset;"
+);
+
+$this->dbs->bind(':charset', DB_CHARSET);
 
 return $this->dbs->execute();
