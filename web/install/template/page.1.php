4c39df177bfa1a236e0a2230e628692c201ae3bc
Marvin Lukaschek <5796460+Groruk@users.noreply.github.com>
2019-10-04 20:27:03 +0200

    Update license headers in web panel code

--- web/install/template/page.1.php
+++ web/install/template/page.1.php
@@ -11,36 +11,28 @@
         <textarea cols="105" rows="32">
 This program is part of SourceBans++.
 
-Copyright © 2014-2019 SourceBans++ Dev Team <https://github.com/sbpp>
+    SourceBans++ (c) 2014-2019 by SourceBans++ Dev Team
 
-SourceBans++ is licensed under a
-GNU GENERAL PUBLIC LICENSE Version 3.
+    The SourceBans++ Web panel is licensed under a
+    Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 
-You should have received a copy of the license along with this
-work.  If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
+    You should have received a copy of the license along with this
+    work.  If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+    This program is based off work covered by the following copyright(s):
+        SourceBans 1.4.11
+        Copyright © 2007-2014 SourceBans Team - Part of GameConnect
+        Licensed under CC-BY-NC-SA 3.0
+        Page: <http://www.sourcebans.net/> - <http://www.gameconnect.net/>
 
-This program is based off work covered by the following copyright(s):
-    SourceBans 1.4.11
-    Copyright © 2007-2014 SourceBans Team - Part of GameConnect
-    Licensed under GPLv3
-     Page: <http://www.sourcebans.net/> - <http://www.gameconnect.net/>
+        SourceComms 0.9.266
+        Copyright (C) 2013-2014 Alexandr Duplishchev
+        Licensed under GNU GPL version 3, or later.
+        Page: <https://forums.alliedmods.net/showthread.php?p=1883705> - <https://github.com/d-ai/SourceComms>
 
-    SourceComms 0.9.266
-    Copyright (C) 2013-2014 Alexandr Duplishchev
-    Licensed under GNU GPL version 3, or later.
-    Page: <https://forums.alliedmods.net/showthread.php?p=1883705> - <https://github.com/d-ai/SourceComms>
-
-    SourceBans TF2 Theme v1.0
-    Copyright © 2014 IceMan
-    Page: <https://forums.alliedmods.net/showthread.php?t=252533>
+        SourceBans TF2 Theme v1.0
+        Copyright © 2014 IceMan
+        Page: <https://forums.alliedmods.net/showthread.php?t=252533>
         </textarea>
         <br /><br />
 
