18cb1ae634cc1a08b49bef3fd3b41baaf6bfc776
Marvin Lukaschek <Groruk@googlemail.com>
2019-10-30 23:08:03 +0100

    Migrate exportbans.php

--- web/exportbans.php
+++ web/exportbans.php
@@ -8,20 +8,18 @@ if (!$userbank->HasAccess(ADMIN_OWNER) && !Config::getBool('config.exportpublic'
     if ($_GET['type'] == 'steam') {
         header('Content-Type: application/x-httpd-php php');
         header('Content-Disposition: attachment; filename="banned_user.cfg"');
-        $bans = $GLOBALS['db']->Execute("SELECT authid FROM `" . DB_PREFIX . "_bans` WHERE length = '0' AND RemoveType IS NULL AND type = '0'");
-        $num  = $bans->RecordCount();
-        for ($x = 0; $x < $num; $x++) {
-            print("banid 0 " . $bans->fields['authid'] . "\r\n");
-            $bans->MoveNext();
+
+        $GLOBALS['PDO']->query("SELECT authid FROM `:prefix_bans` WHERE length = 0 AND RemoveType IS NULL AND type = 0");
+        foreach ($GLOBALS['PDO']->resultset() as $ban) {
+            print("banid 0 $ban[authid]"."\r\n");
         }
     } elseif ($_GET['type'] == 'ip') {
         header('Content-Type: application/x-httpd-php php');
         header('Content-Disposition: attachment; filename="banned_ip.cfg"');
-        $bans = $GLOBALS['db']->Execute("SELECT ip FROM `" . DB_PREFIX . "_bans` WHERE length = '0' AND RemoveType IS NULL AND type = '1'");
-        $num  = $bans->RecordCount();
-        for ($x = 0; $x < $num; $x++) {
-            print("addip 0 " . $bans->fields['ip'] . "\r\n");
-            $bans->MoveNext();
+
+        $GLOBALS['PDO']->query("SELECT ip FROM `:prefix_bans` WHERE length = 0 AND RemoveType IS NULL AND type = 1");
+        foreach($GLOBALS['PDO']->resultset() as $ban) {
+            print("addip 0 $ban[ip]"."\r\n");
         }
     }
 }
