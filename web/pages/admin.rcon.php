800220488b35f261af7b682495ea451d2c966a52
Marvin Lukaschek <Groruk@googlemail.com>
2019-10-30 23:11:02 +0100

    Migrate admin.rcon.php

--- web/pages/admin.rcon.php
+++ web/pages/admin.rcon.php
@@ -29,7 +29,9 @@ new AdminTabs([], $userbank);
 $sid = (int) $_GET['id'];
 
 // Access on that server?
-$servers = $GLOBALS['db']->GetAll("SELECT `server_id`, `srv_group_id` FROM " . DB_PREFIX . "_admins_servers_groups WHERE admin_id = " . $userbank->GetAid());
+$GLOBALS['PDO']->query("SELECT server_id, srv_group_id FROM `:prefix_admins_servers_groups` WHERE admin_id = :aid");
+$GLOBALS['PDO']->bind(':aid', $userbank->GetAid());
+$servers = $GLOBALS['PDO']->resultset();
 $access  = false;
 foreach ($servers as $server) {
     if ($server['server_id'] == $sid) {
@@ -37,7 +39,9 @@ foreach ($servers as $server) {
         break;
     }
     if ($server['srv_group_id'] > 0) {
-        $servers_in_group = $GLOBALS['db']->GetAll("SELECT `server_id` FROM " . DB_PREFIX . "_servers_groups WHERE group_id = " . (int) $server['srv_group_id']);
+        $GLOBALS['PDO']->query("SELECT server_id FROM `:prefix_servers_groups` WHERE group_id = :gid");
+        $GLOBALS['PDO']->bind(':gid', $server['srv_group_id'], \PDO::PARAM_INT);
+        $servers_in_group = $GLOBALS['PDO']->resultset();
         foreach ($servers_in_group as $servig) {
             if ($servig['server_id'] == $sid) {
                 $access = true;
