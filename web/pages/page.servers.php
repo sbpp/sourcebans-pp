b1f51e8be1cb45c9b26d6e7c10187db7f1228817
Marvin Lukaschek <Groruk@googlemail.com>
2019-10-30 23:08:52 +0100

    Migrate page.servers.php

--- web/pages/page.servers.php
+++ web/pages/page.servers.php
@@ -31,29 +31,25 @@ if (!defined('IN_HOME')) {
     }
 }
 
-$res     = $GLOBALS['db']->Execute("SELECT se.sid, se.ip, se.port, se.modid, se.rcon, md.icon FROM " . DB_PREFIX . "_servers se LEFT JOIN " . DB_PREFIX . "_mods md ON md.mid=se.modid WHERE se.sid > 0 AND se.enabled = 1 ORDER BY se.modid, se.sid");
-$servers = array();
-$i       = 0;
-while (!$res->EOF) {
-    $info          = array();
-    $info['sid']   = $res->fields[0];
-    $info['ip']    = $res->fields[1];
-    $info['port']  = $res->fields[2];
-    $info['icon']  = $res->fields[5];
-    $info['index'] = $i;
-    if (defined('IN_HOME')) {
-        $info['evOnClick'] = "window.location = 'index.php?p=servers&s=" . $info['index'] . "';";
+$GLOBALS['PDO']->query(
+    "SELECT se.sid, se.ip, se.port, se.modid, se.rcon, md.icon FROM `:prefix_servers` se
+    LEFT JOIN `:prefix_mods` md ON md.mid = se.modid WHERE se.enabled = 1
+    ORDER BY se.modid, se.sid");
+
+$servers = $GLOBALS['PDO']->resultset();
+
+$home = defined('IN_HOME');
+foreach($servers as $key => $serv) {
+    if ($home) {
+        $servers[$key]['evOnClick'] = "window.location = 'index.php?p=servers&s=$key';";
     }
 
-    $GLOBALS['server_qry'] .= "xajax_ServerHostPlayers({$info['sid']}, 'servers', '', '" . $i . "', '" . $number . "', '" . defined('IN_HOME') . "', 70);";
-    array_push($servers, $info);
-    $i++;
-    $res->MoveNext();
+    $GLOBALS['server_qry'] .= "xajax_ServerHostPlayers($serv[sid], 'servers', ''. '$key', '$number', '$home', 70);";
 }
 
 $theme->assign('access_bans', ($userbank->HasAccess(ADMIN_OWNER | ADMIN_ADD_BAN) ? true : false));
 $theme->assign('server_list', $servers);
-$theme->assign('IN_SERVERS_PAGE', !defined('IN_HOME'));
+$theme->assign('IN_SERVERS_PAGE', !$home);
 $theme->assign('opened_server', $number);
 
 if (!defined('IN_HOME')) {
