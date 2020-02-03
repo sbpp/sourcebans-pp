ae2fa1258c678f1582ae0e89d9feb8570c05ea5e
Jake Forrester <jake@ranndesigns.com>
2020-02-03 12:31:11 -0800

    Cleanup Config

--- web/includes/Config.php
+++ web/includes/Config.php
@@ -1,26 +1,51 @@
 <?php
 
+/**
+ * Class Config
+ */
 class Config
 {
+    /**
+     * @var array
+     */
     private static $config = [];
+
+    /**
+     * @var Database
+     */
     private static $dbh = null;
 
+    /**
+     * @param Database $dbh
+     */
     public static function init(Database $dbh)
     {
         self::$dbh = $dbh;
         self::$config = self::getAll();
     }
 
+    /**
+     * @param string $setting
+     * @return mixed|null
+     */
     public static function get($setting)
     {
         return array_key_exists($setting, self::$config) ? self::$config[$setting] : null;
     }
 
+    /**
+     * @param string $setting
+     * @return bool
+     */
     public static function getBool($setting)
     {
         return (bool)self::get($setting);
     }
 
+    /**
+     * @param int $timestamp
+     * @return false|string
+     */
     public static function time($timestamp)
     {
         $format = self::get('config.dateformat');
@@ -28,6 +53,9 @@ class Config
         return date($format, $timestamp);
     }
 
+    /**
+     * @return array
+     */
     private static function getAll()
     {
         self::$dbh->query("SELECT * FROM `:prefix_settings`");
