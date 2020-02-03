c4f4caebf4e7417557ee1e3434c510a5bc314779
Jake Forrester <jake@ranndesigns.com>
2020-02-03 12:30:19 -0800

    Cleanup Log

--- web/includes/Log.php
+++ web/includes/Log.php
@@ -1,15 +1,34 @@
 <?php
 
+/**
+ * Class Log
+ */
 class Log
 {
+    /**
+     * @var Database
+     */
     private static $dbs = null;
+    /**
+     * @var CUserManager
+     */
     private static $user = null;
-    public static function init(\Database $dbs, \CUserManager $user)
+
+    /**
+     * @param Database $dbs
+     * @param CUserManager $user
+     */
+    public static function init(Database $dbs, CUserManager $user)
     {
         self::$dbs = $dbs;
         self::$user = $user;
     }
 
+    /**
+     * @param string $type
+     * @param string $title
+     * @param string $message
+     */
     public static function add($type, $title, $message)
     {
         $host = filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP) ? $_SERVER['REMOTE_ADDR'] : '';
@@ -28,7 +47,13 @@ class Log
         self::$dbs->execute();
     }
 
-    public static function getAll($start, $limit, $search = null)
+    /**
+     * @param int $start
+     * @param int $limit
+     * @param string $search Entire "WHERE" statement including the word WHERE
+     * @return mixed
+     */
+    public static function getAll($start, $limit, $search)
     {
         $query = "SELECT ad.user, l.* FROM `:prefix_log` AS l
                   LEFT JOIN `:prefix_admins` AS ad ON l.aid = ad.aid
@@ -36,12 +61,16 @@ class Log
                   LIMIT :start, :lim";
         $query = str_replace(':search', filter_var($search, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES), $query);
         self::$dbs->query($query);
-        self::$dbs->bind(':start', (int)$start, \PDO::PARAM_INT);
-        self::$dbs->bind(':lim', (int)$limit, \PDO::PARAM_INT);
+        self::$dbs->bind(':start', (int)$start, PDO::PARAM_INT);
+        self::$dbs->bind(':lim', (int)$limit, PDO::PARAM_INT);
         return self::$dbs->resultset();
     }
 
-    public static function getCount($search = null)
+    /**
+     * @param string $search Entire "WHERE" statement including the word WHERE
+     * @return mixed
+     */
+    public static function getCount($search)
     {
         $query = "SELECT COUNT(l.lid) AS count FROM `:prefix_log` AS l :search";
         $query = str_replace(':search', filter_var($search, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES), $query);
@@ -50,8 +79,12 @@ class Log
         return $log['count'];
     }
 
+    /**
+     * @return string
+     */
     private static function getCaller()
     {
+        $functions = '';
         foreach (debug_backtrace() as $key => $line) {
             $functions .= isset($line[$key]['file']) ? $line[$key]['file'].' - '.$line[$key]['line']."\r\n" : '';
         }
