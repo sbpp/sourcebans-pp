4cd8f489fb9e586c6d19353bf80e1210f717d990
Jake Forrester <jake@ranndesigns.com>
2020-02-03 12:31:36 -0800

    Cleanup SteamID

--- web/includes/SteamID/VanityURL.php
+++ web/includes/SteamID/VanityURL.php
@@ -2,8 +2,17 @@
 
 namespace SteamID;
 
+/**
+ * Class VanityURL
+ * @package SteamID
+ */
 class VanityURL
 {
+    /**
+     * @param string $url
+     * @param string $steamApiKey
+     * @return bool|mixed
+     */
     public static function resolve($url, $steamApiKey)
     {
         $endpoint = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=$steamApiKey&vanityurl=$url";
