533937da768b312d271e025c21e5ae91765a0f17
Connor Richlen <connorrichlen@gmail.com>
2023-09-27 10:36:40 -0500

    Update 706.php

--- web/updater/data/706.php
+++ web/updater/data/706.php
@@ -1,1 +1,11 @@
+<?php
 
+$sql = <<<SQL
+INSERT INTO `:prefix_settings`
+    (`setting`, `value`, `id`) VALUES ('config.enablegametracker', '1', NULL);
+SQL;
+
+$this->dbs->query($sql);
+$this->dbs->execute();
+
+return true;
